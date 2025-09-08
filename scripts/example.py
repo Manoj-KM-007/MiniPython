d7e35ef5f57724ef49d6d004aaefdbe91f350534
Manoj-KM-007 <136242596+Manoj-KM-007@users.noreply.github.com>
2025-09-08 11:37:00 +0530

    Add files via upload

---
+++ scripts/example.py
@@ -0,0 +1,18 @@
+
+#disable VS code extension /editor extensions for seeing this without error
+#constants and const keyword are undefined in actual python
+# bottom left corner -> click {} python -> change to Plain text
+
+#using import (make sure to keep import files (math.py and author.py) in the same directory as MiniPython.exe)
+import math
+import author
+
+radius = 2
+area  =  radius * radius * pi 
+
+printc("Area of the Circle: ")
+print(area)
+printc("Program Written By: ")
+print(author)
+
+print()
